71ca26042218d8fa8f3a0cd8687471a354039d04
Leon Lau <leondotlau@gmail.com>
2019-07-26 19:10:52 +1000

    about page

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,3 +1,32 @@
 <template>
-  <div>Placeholder text</div>
+  <section class="section">
+    <div class="container about">
+      <h1 class="title">About</h1>
+      <h2 class="subtitle">nbaplay has been made with love.</h2>
+      <h2
+        class="subtitle"
+      >This site has been developed to allow tracking of multiple NBA game play by play's, and as a project to learn a few technologies along the way.</h2>
+      <h2 class="subtitle">
+        For those interested this site was built using:
+        <ul>
+          <li>Vue.js</li>
+          <li>Node.js</li>
+          <li>Google Firestore</li>
+        </ul>
+      </h2>
+      <h2 class="subtitle">I hope you find this website useful :)</h2>
+      <h2 class="subtitle">Leon</h2>
+
+      <h2 class="subtitle">Note:</h2>
+      <h2 class="subtitle">All images on this website are the property of their respective owners.</h2>
+    </div>
+  </section>
 </template>
+
+<style lang="scss" scoped>
+.about {
+  background: #cccccc;
+  border-radius: 5px;
+  padding: 3rem;
+}
+</style>
