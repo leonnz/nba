74fcd6a5b7d6d5755e5999968912078f498be585
Leon Lau <leondotlau@gmail.com>
2019-10-14 18:56:28 +1100

    refactored

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -32,46 +32,63 @@ const initGameService = function() {
   });
 };
 
+const numberUnfinishedGames = function(todaysGames) {
+  let unfinishedGames = 0;
+
+  for (var key in todaysGames) {
+    if (todaysGames[key].statusNum !== 3) {
+      unfinishedGames++;
+    }
+  }
+
+  return unfinishedGames;
+};
+
 const updateGameService = function() {
   axios.get('/prod/v3/today.json').then(response => {
     // const date = response.data.links.currentDate;
     const date = '20191011';
     store.commit('addGameDate', date);
 
-    setInterval(() => {
-      axios.get(`/prod/v2/${date}/scoreboard.json`).then(response => {
-        response.data.games.forEach(game => {
-          const gameStore = store.getters.getTodaysGames[game.gameId];
-          let updateGamePayload = { gameId: game.gameId };
+    const todaysGames = store.getters.getTodaysGames;
 
-          if (game.isGameActivated !== gameStore.isGameActivated) {
-            updateGamePayload.isGameActivated = game.isGameActivated;
-          }
-          if (game.statusNum !== gameStore.statusNum) {
-            updateGamePayload.statusNum = game.statusNum;
-          }
-          if (game.clock !== gameStore.clock) {
-            updateGamePayload.clock = game.clock;
-          }
-          if (game.endTimeUTC !== gameStore.endTimeUTC) {
-            updateGamePayload.endTimeUTC = game.endTimeUTC;
-          }
-          if (game.period.current !== gameStore.period) {
-            updateGamePayload.period = game.period;
-          }
-          if (game.vTeam.score !== gameStore.vTeamScore) {
-            updateGamePayload.vTeamScore = game.vTeam.score;
-          }
-          if (game.hTeam.score !== gameStore.hTeamScore) {
-            updateGamePayload.hTeamScore = game.hTeam.score;
-          }
-          if (Object.keys(updateGamePayload).length > 1) {
-            store.commit('updateGame', updateGamePayload);
-          }
+    if (numberUnfinishedGames(todaysGames) > 0) {
+      setInterval(() => {
+        axios.get(`/prod/v2/${date}/scoreboard.json`).then(response => {
+          response.data.games.forEach(game => {
+            const gameStore = store.getters.getTodaysGames[game.gameId];
+
+            let updateGamePayload = { gameId: game.gameId };
+
+            if (game.isGameActivated !== gameStore.isGameActivated) {
+              updateGamePayload.isGameActivated = game.isGameActivated;
+            }
+            if (game.statusNum !== gameStore.statusNum) {
+              updateGamePayload.statusNum = game.statusNum;
+            }
+            if (game.clock !== gameStore.clock) {
+              updateGamePayload.clock = game.clock;
+            }
+            if (game.endTimeUTC !== gameStore.endTimeUTC) {
+              updateGamePayload.endTimeUTC = game.endTimeUTC;
+            }
+            if (game.period.current !== gameStore.period) {
+              updateGamePayload.period = game.period;
+            }
+            if (game.vTeam.score !== gameStore.vTeamScore) {
+              updateGamePayload.vTeamScore = game.vTeam.score;
+            }
+            if (game.hTeam.score !== gameStore.hTeamScore) {
+              updateGamePayload.hTeamScore = game.hTeam.score;
+            }
+            if (Object.keys(updateGamePayload).length > 1) {
+              store.commit('updateGame', updateGamePayload);
+            }
+          });
         });
-      });
-      console.log('this ran');
-    }, 5000);
+        console.log('this ran');
+      }, 5000);
+    }
   });
 };
 
